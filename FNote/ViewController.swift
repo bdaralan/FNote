89389060edb2d6c0f2677e4b93f66929fd303fc0
bdaralan <daralan@daras-macbook-pro.local>
2019-01-17 21:10:01 -0800

    Initial Commit

---
+++ FNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FNote
+//
+//  Created by Dara Beng on 1/17/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
