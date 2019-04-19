e6e8b8b35dbbeb2fe6fb553706fc1057ad966162
Dara <bdaralan@gmail.com>
2019-04-19 14:43:40 -0700

    Add convenient initializer to get color from color hex

--- FNote/Extension+Utility/Image+Color.swift
+++ FNote/Extension+Utility/Image+Color.swift
@@ -40,3 +40,40 @@ extension UIColor {
     static let vocabularyFavoriteStarTrue = UIColor(named: "favorite-vocab-true")
     static let vocabularyFavoriteStarFalse = UIColor(named: "favorite-vocab-false")
 }
+
+
+extension UIColor {
+    
+    /// A string of six lowercase or uppercase characters without #.
+    typealias ColorHex = String
+    
+    static private let colorHexValue: [Character: Int] = {
+        return [
+            "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+            "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15
+        ]
+    }()
+    
+    convenience init(colorHex: ColorHex) {
+        guard colorHex.count == 6 else {
+            self.init(white: 1, alpha: 1)
+            return
+        }
+        
+        let colorHex = colorHex.uppercased()
+        
+        let firstRed = UIColor.colorHexValue[colorHex[0]] ?? 0
+        let secondRed = UIColor.colorHexValue[colorHex[1]] ?? 0
+        let red = CGFloat(firstRed * 16 + secondRed) / 255
+        
+        let firstGreen = UIColor.colorHexValue[colorHex[2]] ?? 0
+        let secondGreen = UIColor.colorHexValue[colorHex[3]] ?? 0
+        let green = CGFloat(firstGreen * 16 + secondGreen) / 255
+        
+        let firstBlue = UIColor.colorHexValue[colorHex[4]] ?? 0
+        let secondBlue = UIColor.colorHexValue[colorHex[5]] ?? 0
+        let blue = CGFloat(firstBlue * 16 + secondBlue) / 255
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
