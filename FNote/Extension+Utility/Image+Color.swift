6bcf933100223b06b54450164fdd82f6b6be7634
Dara <bdaralan@gmail.com>
2019-04-20 08:54:19 -0700

    Update color from hex logic

--- FNote/Extension+Utility/Image+Color.swift
+++ FNote/Extension+Utility/Image+Color.swift
@@ -44,7 +44,7 @@ extension UIColor {
 
 extension UIColor {
     
-    /// A string of six lowercase or uppercase characters without #.
+    /// A six-character string, 0-9 and A-F. The letters are case insensitive.
     typealias ColorHex = String
     
     static private let colorHexValue: [Character: Int] = {
@@ -60,19 +60,25 @@ extension UIColor {
             return
         }
         
-        let colorHex = colorHex.uppercased()
+        let uppercasedHex = colorHex.uppercased()
+        let r1Hex = uppercasedHex[0]
+        let r2Hex = uppercasedHex[1]
+        let g1Hex = uppercasedHex[2]
+        let g2Hex = uppercasedHex[3]
+        let b1Hex = uppercasedHex[4]
+        let b2Hex = uppercasedHex[5]
+        let values = UIColor.colorHexValue
         
-        let firstRed = UIColor.colorHexValue[colorHex[0]] ?? 0
-        let secondRed = UIColor.colorHexValue[colorHex[1]] ?? 0
-        let red = CGFloat(firstRed * 16 + secondRed) / 255
+        guard let red1 = values[r1Hex], let red2 = values[r2Hex],
+            let green1 = values[g1Hex], let green2 = values[g2Hex],
+            let blue1 = values[b1Hex], let blue2 = values[b2Hex] else {
+            self.init(white: 1, alpha: 1)
+            return
+        }
         
-        let firstGreen = UIColor.colorHexValue[colorHex[2]] ?? 0
-        let secondGreen = UIColor.colorHexValue[colorHex[3]] ?? 0
-        let green = CGFloat(firstGreen * 16 + secondGreen) / 255
-        
-        let firstBlue = UIColor.colorHexValue[colorHex[4]] ?? 0
-        let secondBlue = UIColor.colorHexValue[colorHex[5]] ?? 0
-        let blue = CGFloat(firstBlue * 16 + secondBlue) / 255
+        let red = CGFloat(red1 * 16 + red2) / 255
+        let green = CGFloat(green1 * 16 + green2) / 255
+        let blue = CGFloat(blue1 * 16 + blue2) / 255
         
         self.init(red: red, green: green, blue: blue, alpha: 1)
     }
