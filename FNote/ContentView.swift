58ecbad27525df08d3173a590ea8ac96c6ed9f09
Dara <bdaralan@gmail.com>
2019-08-26 11:34:49 -0700

    Initial Commit

---
+++ FNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FNote
+//
+//  Created by Dara Beng on 8/26/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
