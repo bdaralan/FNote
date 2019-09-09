398410a4a4c87b021c91b7e4c2cc2a9b4d857eb9
Dara <bdaralan@gmail.com>
2019-09-09 12:51:47 -0700

    Add tab items

--- FNote/View/MainTabView.swift
+++ FNote/View/MainTabView.swift
@@ -10,8 +10,57 @@ import SwiftUI
 
 
 struct MainTabView: View {
+    
+    @State private var currentTabItem = Tab.home
+    
+    var currentTabItemTag: Binding<Int> {
+        .init(
+            get: { self.currentTabItem.rawValue },
+            set: { self.currentTabItem = Tab(rawValue: $0)! }
+        )
+    }
+    
     var body: some View {
-        Text("Main Tab View")
+        TabView(selection: currentTabItemTag) {
+            Text("Home").tabItem {
+                createTabItem(name: "Notes", image: Image(systemName: "rectangle.fill.on.rectangle.angled.fill"))
+            }
+            .tag(Tab.home.rawValue)
+            
+            Text("Collections").tabItem {
+                createTabItem(name: "Collections", image: Image(systemName: "rectangle.stack.fill"))
+            }
+            .tag(Tab.collection.rawValue)
+            
+            Text("Tags").tabItem {
+                createTabItem(name: "Tags", image: Image(systemName: "tag.fill"))
+            }
+            .tag(Tab.tag.rawValue)
+            
+            Text("Settings").tabItem {
+                createTabItem(name: "Settings", image: Image(systemName: "gear"))
+            }
+            .tag(Tab.setting.rawValue)
+        }
+    }
+}
+
+
+extension MainTabView {
+    
+    func createTabItem(name: String, image: Image) -> some View {
+        ViewBuilder.buildBlock(image, Text(name))
+    }
+}
+
+
+extension MainTabView {
+    
+    enum Tab: Int {
+        case home
+        case collection
+        case tag
+        case setting
     }
 }
 
