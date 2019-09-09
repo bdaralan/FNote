3631280b7a48b32d37cde2009c1ac15ddb70c91e
Dara <bdaralan@gmail.com>
2019-09-09 12:05:39 -0700

    Add CoreDataStack and AppCache object

---
+++ FNote/CoreDataStack/AppCache.swift
@@ -0,0 +1,68 @@
+//
+//  AppCache.swift
+//  FNote
+//
+//  Created by Dara Beng on 9/9/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+/// An object that provide access to application's values stored in `UserDefaults`.
+struct AppCache {
+    
+    typealias UbiquityIdentityToken = (NSCoding & NSCopying & NSObjectProtocol)
+    
+    
+    @UserDefaultsOptionalValue(forKey: "AppCache.kUbiquityIdentityToken", default: nil)
+    static var ubiquityIdentityToken: UbiquityIdentityToken?
+    
+    @UserDefaultsOptionalValue(forKey: "AppCache.kCurrentStoreURIData", default: nil)
+    static var currentStoreURIData: Data?
+}
+
+
+// MARK: - Non Optional Value
+
+@propertyWrapper
+struct UserDefaultsValue<T> {
+    
+    let key: String
+    
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+        get { UserDefaults.standard.value(forKey: key) as? T ?? defaultValue }
+    }
+    
+    
+    init(forKey key: String, default value: T) {
+        self.key = key
+        self.defaultValue = value
+    }
+}
+
+
+// MARK: - Optional Value
+
+@propertyWrapper
+struct UserDefaultsOptionalValue<T> {
+    
+    let key: String
+    
+    let defaultValue: T?
+    
+    var wrappedValue: T? {
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+        get { UserDefaults.standard.value(forKey: key) as? T}
+    }
+    
+    
+    init(forKey key: String, default value: T?) {
+        self.key = key
+        self.defaultValue = value
+    }
+}
---
+++ FNote/CoreDataStack/CoreDataStack.swift
@@ -0,0 +1,82 @@
+//
+//  CoreDataStack.swift
+//  FNote
+//
+//  Created by Dara Beng on 9/9/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+// reference: https://developer.apple.com/documentation/coredata/consuming_relevant_store_changes
+
+import CoreData
+
+
+/// The core data stack that manages user's object graph.
+class CoreDataStack: NSObject {
+    
+    static private(set) var current = CoreDataStack()
+    
+    
+    let persistentContainer: NSPersistentContainer
+    
+    var mainContext: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    
+    private override init() {
+        // use CloudKit container
+        persistentContainer = NSPersistentCloudKitContainer(name: "FNote")
+        
+        // auto merge new changes when store gets new updates
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
+        
+        // load container
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            if let error = error { fatalError("could not load persistent store with error: \(error)") }
+        }
+        
+        super.init()
+        
+        AppCache.ubiquityIdentityToken = FileManager.default.ubiquityIdentityToken
+        setupUserIdentityChangeNotification()
+    }
+}
+
+
+// MARK: - Handle Identity Changed
+
+extension CoreDataStack {
+    
+    static let nCoreDataStackDidChange = Notification.Name("CoreDataStack.nCoreDataStackDidChange")
+    
+    
+    func setupUserIdentityChangeNotification() {
+        let notificationCenter = NotificationCenter.default
+        let notification = Notification.Name.NSUbiquityIdentityDidChange
+        let handler = #selector(userIdentifyChanged(_:))
+        notificationCenter.addObserver(self, selector: handler, name: notification, object: nil)
+    }
+    
+    @objc func userIdentifyChanged(_ notification: Notification) {
+        let cachedToken = AppCache.ubiquityIdentityToken
+        let currentToken = FileManager.default.ubiquityIdentityToken
+        
+        AppCache.ubiquityIdentityToken = currentToken // update cache
+        
+        switch (cachedToken == nil, currentToken == nil) {
+            
+        case (false, true), (true, false):
+            reloadCoreDataStore()
+            
+        case (false, false) where !cachedToken!.isEqual(currentToken!):
+            reloadCoreDataStore()
+            
+        default: break
+        }
+    }
+    
+    func reloadCoreDataStore() {
+        CoreDataStack.current = CoreDataStack()
+        NotificationCenter.default.post(name: CoreDataStack.nCoreDataStackDidChange, object: nil)
+    }
+}
