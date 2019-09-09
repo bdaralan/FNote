4f68d0c58ce795cf6341b0ea4bdd56b47cb15bec
Dara <bdaralan@gmail.com>
2019-09-09 12:07:03 -0700

    Create MainTabView

---
+++ FNote/View/MainTabView.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabView.swift
+//  FNote
+//
+//  Created by Dara Beng on 9/9/19.
+//  Copyright © 2019 Dara Beng. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct MainTabView: View {
+    var body: some View {
+        Text("Main Tab View")
+    }
+}
+
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
