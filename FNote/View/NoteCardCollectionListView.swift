317854dc6ae062389f2c65cfe6b99fb635419f9b
Brittney Witts <brittneywitts@hotmail.com>
2019-09-16 12:50:39 -0700

    Created context menu and implemented deleteCollection function

--- FNote/View/NoteCardCollectionListView.swift
+++ FNote/View/NoteCardCollectionListView.swift
@@ -2,7 +2,7 @@
 //  NoteCardCollectionListView.swift
 //  FNote
 //
-//  Created by Brittney Witts on 9/11/19.
+//  Created by Brittney Witts on 9/16/19.
 //  Copyright © 2019 Dara Beng. All rights reserved.
 //
 
@@ -10,29 +10,43 @@ import SwiftUI
 
 struct NoteCardCollectionListView: View {
     
-    var sampleCollection = NoteCardCollection.sampleCollections(count: 10, noteCount: 1)
+    @State var sampleCollection = NoteCardCollection.sampleCollections(count: 10, noteCount: 1)
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(sampleCollection, id: \.self) { collection in
-                    Button(action: { self.collectionTapped(collection) }) { //
-                        Text(collection.name) // button label
+                    Text(collection.name)
+                    
+                        .contextMenu {
+                            Button(action: { self.renameCollection(collection) }) {
+                                Text("Rename")
+                                Image(systemName: "square.and.pencil")
+                            }
+                            Button(action: { self.deleteCollection(collection) }) {
+                                Text("Delete")
+                                Image(systemName: "trash")
+                            }
+                        }
                     }
                 }
-            }
             .navigationBarTitle("Collections")
         }
     }
 }
 
-// button action
-// needs to be selected and have a check mark on the right side
 extension NoteCardCollectionListView {
-    
-    func collectionTapped(_ collection: NoteCardCollection) {
-        
+
+    func renameCollection(_ collection: NoteCardCollection) {
+        // rename the collection to what the user wants
     }
+    
+    func deleteCollection(_ collection: NoteCardCollection) {
+        // delete the collection
+        let index = sampleCollection.firstIndex(of: collection)!
+        sampleCollection.remove(at: index)
+    }
+    
 }
 
 struct NoteCardCollectionListView_Previews: PreviewProvider {
